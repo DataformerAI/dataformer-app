9a3d67d28f7950763817ba904122d7793bf93dcf
Satpal Singh Rathore <satpal.code@gmail.com>
2024-06-13 16:37:58 +0530

    hf username optional

--- src/backend/base/dfapp/components/data/PushToHub.py
+++ src/backend/base/dfapp/components/data/PushToHub.py
@@ -11,7 +11,7 @@ class PushToHubComponent(CustomComponent):
             "user_name": {"display_name": "hf user name", "info": "Hugging Face username."},
             "dataset_name": {
                 "display_name": "hf dataset name",
-                "info": "Name of the dataset to push. If it includes a '/', it will be used as the full repo_id.",
+                "info": "Name of the dataset to push. Specify username in user_name column if not included here.",
                 "required": True
                 },
             "huggingface_token": {
@@ -22,7 +22,7 @@ class PushToHubComponent(CustomComponent):
             },
         }
 
-    def build(self, dataset: DatasetDict, user_name: str, dataset_name: str, huggingface_token: str):
+    def build(self, dataset: DatasetDict, dataset_name: str, huggingface_token: str, user_name: str = None):
         if '/' in dataset_name:
             repo_id = dataset_name
         else:
